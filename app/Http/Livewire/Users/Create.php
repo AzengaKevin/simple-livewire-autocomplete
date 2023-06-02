d40ba49b4d09c235a96a879d34df76194d7e3180
Azenga Kevin <azenga.kevin7@gmail.com>
2023-06-03 01:40:27 +0300

    WIP: Added a basic users autocomplete

--- app/Http/Livewire/Users/Create.php
+++ app/Http/Livewire/Users/Create.php
@@ -2,22 +2,57 @@
 
 namespace App\Http\Livewire\Users;
 
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Create extends Component
 {
     public $name;
-    public $results;
-    public $showUserSuggestions;
+    public $users;
+    public $suggestionsFlag;
 
-    public function updatedName($value)
+    public function __construct()
     {
-        info($value);
+        $this->suggestionsFlag = false;
+        $this->users = Collection::empty();
     }
 
-    public function submit()
+    public function mount()
     {
-        info($this->name);
+        $this->getUsers();
+    }
+
+    public function updatedName()
+    {
+        $this->getUsers();
+    }
+
+    public function getUsers()
+    {
+        $this->users = \App\Models\User::query()->when($this->name, function ($innerQuery, $name) {
+            $innerQuery->where("name", "like", "%{$name}%");
+        })->orderBy("name")
+            ->limit(12)
+            ->get(["id", "name"]);
+    }
+
+    public function selectUser($id)
+    {
+        $selectedUser = $this->users->where("id", $id)->first();
+
+        $this->name = $selectedUser->name;
+
+        $this->suggestionsFlag = false;
+    }
+
+    public function showSuggestions()
+    {
+        $this->suggestionsFlag = true;
+    }
+
+    public function hideSuggestions()
+    {
+        $this->suggestionsFlag = false;
     }
 
     public function render()
--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -1,13 +1,31 @@
-<form wire:submit.prevent="submit" class="space-y-4">
+<form wire:submit.prevent="submit" class="space-y-4" x-data>
     <div class="form-group">
         <label for="user" class="block text-sm font-medium text-gray-900">Select User</label>
-        <div class="flex flex-col mt-2">
-            <input type="text" id="user" wire:model="name"
+        <div class="mt-2 relative">
+            <input type="text" id="user" wire:model="name" autocomplete="off" @focus="$wire.showSuggestions()"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 placeholder="Search user..." required>
+
+            @if ($suggestionsFlag)
+                <ul class="absolute z-10 bg-white w-full start-0 end-0 h-72 overflow-y-auto" @click.outside="$wire.hideSuggestions()">
+                    @if ($users->count())
+                        @foreach ($users as $user)
+                            <li class="border-b"><button type="button"
+                                    wire:click.prevent="selectUser({{ $user->id }})"
+                                    class="px-2.5 py-2 w-full text-start bg-gray-50 hover:bg-gray-100">{{ $user->name }}</button>
+                            </li>
+                        @endforeach
+                    @else
+                        <li><button type="button"
+                                class="px-2.5 py-2 w-full text-start bg-gray-50 hover:bg-gray-100">Add
+                                "{{ $name }}"</button></li>
+                    @endif
+                </ul>
+            @endif
         </div>
     </div>
     <div class="form-group">
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Submit</button>
+        <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Submit</button>
     </div>
 </form>
